b79d3030650a60e05510e5fc1b09f36689e01000
Ivan herald <ivanheraldd@gmail.com>
2023-12-28 18:49:10 +0530

    first commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './styles/Navbar.css'
+const Navbar = () => {
+  return (
+    <div className="home">
+      <nav className="navbar">
+      <ul className="navbar-nav">
+        <br/>
+    <li className="nav-item">
+    <a href="/" className="nav-link">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-pie-chart"><path d="M21.21 15.89A10 10 0 1 1 8 2.83"></path><path d="M22 12A10 10 0 0 0 12 2v10z"></path></svg>
+        <span className="link-text">Home</span>
+      </a>
+    </li>
+    
+    <li className="nav-item">
+      <a href="/create" className="nav-link">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+        <span className="link-text">Create New Issue</span>
+      </a>
+    </li>
+    <li className="nav-item">
+      <a href="/tickets" className="nav-link">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-layers"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
+        <span className="link-text">View Issues</span>
+      </a>
+    </li>
+    
+  </ul>
+</nav>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/component/Tracker.js
@@ -0,0 +1,147 @@
+import React, { useState, useEffect } from 'react';
+import { TextField,InputLabel,Select,MenuItem ,Button,Snackbar, FormControl} from '@mui/material';
+import './styles/Tracker.css'
+import Navbar from './Navbar';
+function Tracker() {
+  const [currentDateTime, setCurrentDateTime] = useState('');
+  const [issueId,setIssueId] = useState(1)
+  const [openBy,setOpenBy] = useState('')
+  const [requestedFor,setrequestedFor] = useState('')
+  const [description,setdescription] = useState('')
+  const [priority,setPriority] = useState('')
+  const [status,setStatus] = useState('')
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+
+  const showSnackbar = (message) => {
+    setSnackbarMessage(message);
+    setSnackbarOpen(true);
+  };
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
+
+
+  useEffect(() => {
+    const getCurrentDateTime = () => {
+      const now = new Date();
+      const formattedDateTime = `${now.toDateString()} ${now.toLocaleTimeString()}`;
+      setCurrentDateTime(formattedDateTime);
+    };
+
+
+    getCurrentDateTime();
+  }, []);
+
+  const handleSubmit=async()=>{
+    if(!issueId ||!currentDateTime||!openBy||!requestedFor||!description||!priority||!status)
+    {
+      showSnackbar('Please fill in all the fields')
+      return
+    }
+    console.log(issueId,currentDateTime,openBy,requestedFor,description,priority,status)
+    const lastTicketData={
+      issueId,
+      openOn:currentDateTime,
+      openBy,
+      requestedFor,
+      description,
+      priority,
+      status
+    }
+    console.log(lastTicketData)
+
+    try{
+      const response = await fetch(`http://localhost:5000/ticket/createTicket`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(lastTicketData),
+      });
+
+      const data = await response.json();
+      console.log('Response from server:', data);
+      if (response.status === 201) {
+        showSnackbar('Ticket saved Successfully');
+        setTimeout(() => {
+          //history('/home',{ replace: true }); 
+        },3000)
+      }
+    }
+    catch(error)
+    {
+      console.log('error',error);
+    }
+
+    setIssueId('')
+    setOpenBy('')
+    setrequestedFor('')
+    setdescription('')
+    setPriority('')
+    setStatus('')
+
+  }
+
+  return (
+<div className='main-container'>
+    <Navbar/>
+    <div className='tracker-container' >
+        
+      <h1>Issue Tracker</h1>
+      <TextField variant="outlined" label="Issue Id" value={issueId} onChange={(e)=>setIssueId(e.target.value)}></TextField><br /><br />
+      <TextField variant="outlined" label="Opened on" value={currentDateTime} InputProps={{
+        readOnly: true,
+      }}></TextField><br /><br />
+      <TextField variant="outlined" label="Opened by" value={openBy} onChange={(e)=>setOpenBy(e.target.value)}></TextField><br /><br />
+      <TextField variant="outlined" label="Requested For" value={requestedFor} onChange={(e)=>setrequestedFor(e.target.value)}></TextField><br /><br />
+     
+      
+      
+      <FormControl sx={{width:"210px"}}>
+
+      <InputLabel id="priority-label">Priority</InputLabel>
+      <Select
+        labelId="priority-label"
+        label="Priority"
+        fullWidth
+        value={priority}
+        onChange={(e)=>setPriority(e.target.value)}
+      >
+        <MenuItem value={'High'}>High</MenuItem>
+        <MenuItem value={'Medium'}>Medium</MenuItem>
+        <MenuItem value={'Low'}>Low</MenuItem>
+      </Select>
+      </FormControl >
+       <br/><br/>
+       <FormControl sx={{width:"210px"}}>
+       <InputLabel id="Status-label">Status</InputLabel>
+       <Select
+       labelId="Status-label"
+        label="Status"
+        fullWidth
+        value={status}
+        onChange={(e)=>setStatus(e.target.value)}
+      >
+        <MenuItem value={'Open'}>Open</MenuItem>
+        <MenuItem value={'Close'}>Close</MenuItem>
+      </Select>
+      </FormControl >
+
+        <br/><br/>
+        <TextField variant="outlined" label="Description" value={description} multiline maxRows={4}fullWidth onChange={(e)=>setdescription(e.target.value)}></TextField><br /><br />
+      <Button variant='outlined' onClick={handleSubmit}>Submit</Button>
+      <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={6000}
+          onClose={handleSnackbarClose}
+          message={snackbarMessage}
+          anchorOrigin={{ vertical: 'center', horizontal: 'center' }}
+        />
+    </div>
+    </div>
+  );
+}
+
+export default Tracker;
